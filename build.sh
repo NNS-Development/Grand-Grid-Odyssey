3fb2dc52d35bdc9c4ed378098ee19178fb8ac3fc
luuna <luna@hackclub.app>
2025-02-12 10:50:57 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ pyinstaller --onefile main.py -
 echo "purging ./dist/"
 rm -rf dist/
 
-python -m PyInstaller --onefile --name "tictactoe" main.py
+python -m PyInstaller --onefile --name "tictactoe-$(arch)" main.py
 
-echo "executable at dist/tictactoe"
+echo "executable at dist/tictactoe-$(arch)"
